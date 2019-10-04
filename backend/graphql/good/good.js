d9e2582308c699ee4c4bca320b0b08b4cdf615c9
Karl-Gustav Kallasmaa <karl.gustav1789@gmail.com>
2019-10-04 16:41:46 +0300

    bug-fixes (#3)

--- backend/graphql/good/good.js
+++ backend/graphql/good/good.js
@@ -22,9 +22,6 @@ const stopwords = require('stopword');
 const stripe = require('stripe')(process.env.STRIPE_API_SECRET);
 
 
-
-
-
 function get_keywords(title, description, general_category_name, seller_name) {
     let titles_as_array = stopwords.removeStopwords(title.split(" ").join('|').toLowerCase().split('|').filter(n => n));
     //TODO: get meaningful keywords
@@ -36,13 +33,13 @@ module.exports = {
     addPhysicalGood: async args => {
         try {
             //Find the seller
-            const KEY = process.env.BUSINESS_JWT_KEY;
-            let decoded = jwt.decode(args.goodInput.seller_jwt_token, KEY);
+            let decoded = jwt.decode(args.goodInput.seller_jwt_token, process.env.BUSINESS_JWT_KEY);
             //Check if the user_id is valid
             const seller = await BusinessUser.findById(decoded.userId);
             if (!seller) {
-                throw new Error('Seller does not exists.');
+                return new Error('Seller does not exists.');
             }
+
             const general_category = await GeneralCategory.findOne({name: args.goodInput.general_category_name});
             if (!general_category) {
                 throw new Error('General category was not found');
@@ -57,17 +54,11 @@ module.exports = {
             //TODO: extract meaningful keywords
 
             //Goods have a cloudinary prefix "good_pictures/"
-            const reformatted_main_image_cloudinary_id = "good_pictures/" + args.goodInput.main_image_cloudinary_public_id;
 
-            //TODO: implement map funcion
-            let reformatted_other_images_array = Array();
-            for (let index = 0; index < args.goodInput.other_images_cloudinary_public_id.length; index++) {
-                const element = "good_pictures/" + args.goodInput.other_images_cloudinary_public_id[index];
-                reformatted_other_images_array.push(element);
-            }
 
             const custom_attribute_names = Array(args.goodInput.custom_attribute_1_name, args.goodInput.custom_attribute_2_name, args.goodInput.custom_attribute_3_name, args.goodInput.custom_attribute_4_name, args.goodInput.custom_attribute_5_name);
             const custom_attribute_values = Array(args.goodInput.custom_attribute_1_value, args.goodInput.custom_attribute_2_value, args.goodInput.custom_attribute_3_value, args.goodInput.custom_attribute_4_value, args.goodInput.custom_attribute_5_value);
+
 
             //Filtering values
             const filtered_custom_attribute_names = custom_attribute_names.filter(function (el) {
@@ -87,21 +78,23 @@ module.exports = {
                 return Math.round((0.035274 * gram) * 100) / 100;
             };
 
+            let count = 0;
+            const allGoods = await Good.find();
+            if (allGoods) {
+                count += allGoods.length;
+            }
 
             //Add good to our database
             const new_good = new Good({
-                nr:await Good.find().length+1,
+                nr: count + 1,
                 title: args.goodInput.title,
                 description: args.goodInput.description,
                 quantity: +args.goodInput.quantity,
-                booked: 0, //When the good is added then no orders have been made
-                current_price: +args.goodInput.current_price,
+                current_price: +args.goodInput.listing_price,
                 listing_price: +args.goodInput.listing_price,
-                listing_timestamp: +parseInt(args.goodInput.listing_timestamp, 10),
                 general_category: general_category._id,
-                main_image_cloudinary_public_id: reformatted_main_image_cloudinary_id,
                 main_image_cloudinary_secure_url: args.goodInput.main_image_cloudinary_secure_url,
-                other_images_cloudinary_public_id: reformatted_other_images_array,
+                other_images_cloudinary_secure_url: args.goodInput.other_images_cloudinary_secure_url,
                 currency: args.goodInput.currency,
                 seller: seller,
                 height_in: millimeter_to_in(args.goodInput.height_mm),
@@ -109,8 +102,6 @@ module.exports = {
                 width_in: millimeter_to_in(args.goodInput.width_mm),
                 weight_oz: gram_to_oz(args.goodInput.weight_g),
             });
-
-//TODO: add past data to mongo
 
 
             if (custom_attribute_names.length === custom_attribute_values.length && (custom_attribute_values.length !== 0 && custom_attribute_names !== 0)) {
@@ -146,16 +137,18 @@ module.exports = {
             }
 
             //Add product to Stripes database
+           // const formated_title = new_good.title.split(' ').join('%');
+           // const url = "http://rocketnow.eu/goods/" + new_good.nr + "/" + formated_title; TODO: add url
             const product = await stripe.products.create({
                 id: new_good.id,
                 name: new_good.title,
                 type: 'good',
                 description: new_good.description,
                 package_dimensions: {
-                    height: result.height_in,
-                    length: result.length_in,
-                    weight: result.weight_oz,
-                    width: result.width_in
+                    height:result.height_in,
+                    length:result.length_in,
+                    weight:result.weight_oz,
+                    width:result.width_in
                 },
                 images: image_array,
                 metadata: {
@@ -163,10 +156,10 @@ module.exports = {
                     SellerEmail: seller.email
                 }
             });
-            console.log("Product" + JSON.stringify(product));
 
-            //Addding sku infonormation
-            const sku = await stripe.skus.create({
+            //TODO: debug
+            /*
+                  const sku = await stripe.skus.create({
                 id: new_good.id,
                 product: product.id,
                 image: args.goodInput.main_image_cloudinary_secure_url,
@@ -183,28 +176,30 @@ module.exports = {
                     type: "finite",
                 }
             });
-            console.log("sku:" + JSON.stringify(sku));
+            console.log(sku);
+             */
 
+            console.log("New good #" + result.id + " was successfully added to the db");
             return transformGood(result);
         } catch
             (err) {
             throw err;
         }
     },
-    individualGood: async ({nr,jwt_token}) => {
-        const good = await Good.findOne({nr:nr});
-        if (!good){
+    individualGood: async ({nr, jwt_token}) => {
+        const good = await Good.findOne({nr: nr});
+        if (!good) {
             return Error("Good was not found");
         }
         //TODO: integrate reccomendations
         return transformGood(good);
     },
-    businessUserGoods: async ({nr,businessname}) => {
-        const user = await BusinessUser.findOne({nr:nr,businessname:businessname});
+    businessUserGoods: async ({nr, displayname}) => {
+        const user = await BusinessUser.findOne({nr: nr, displayname: displayname});
         if (!user) {
             return Error('This business does not exist');
         }
-        const goods= await Good.find({seller:user});
+        const goods = await Good.find({seller: user});
 
         return goods.map(good => {
             return transformGood(good);
